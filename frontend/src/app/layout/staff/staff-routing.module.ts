010be9fdb52c87ab3b14ca7b7f7d167be92ccad1
dungtvdotcom <43840068+dungtvdotcom@users.noreply.github.com>
2019-01-25 15:23:09 +0700

    staff module: add member & permissions component

--- frontend/src/app/layout/staff/staff-routing.module.ts
+++ frontend/src/app/layout/staff/staff-routing.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { StaffComponent } from './staff.component';
+import { MemberComponent } from './member/member.component';
+import { PermissionComponent } from './permission/permission.component';
 
 const routes: Routes = [
     {
-        path: '', component: StaffComponent
+        path: '', component: StaffComponent, children: [
+            {
+                path: '',   redirectTo: 'member', pathMatch: 'full',
+            },
+            {
+                path: 'member',
+                component: MemberComponent
+            },
+            {
+                path: 'permissions',
+                component: PermissionComponent
+            },
+          ]
     }
 ];
 
--- frontend/src/app/layout/staff/staff.module.ts
+++ frontend/src/app/layout/staff/staff.module.ts
@@ -7,9 +7,11 @@ import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { StaffComponent } from './staff.component';
+import { MemberComponent } from './member/member.component';
+import { PermissionComponent } from './permission/permission.component';
 
 @NgModule({
     imports: [CommonModule, StaffRoutingModule, PageHeaderModule,FormsModule, NgbModule],
-    declarations: [StaffComponent]
+    declarations: [StaffComponent, MemberComponent, PermissionComponent]
 })
 export class StaffModule { }
