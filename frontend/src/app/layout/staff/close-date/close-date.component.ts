17b1a90586e304eefdeea2f914aa659f905eb126
dungtvdotcom <43840068+dungtvdotcom@users.noreply.github.com>
2019-02-12 14:05:38 +0700

    update closedate component

--- frontend/src/app/layout/staff/close-date/close-date.component.ts
+++ frontend/src/app/layout/staff/close-date/close-date.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./close-date.component.scss']
 })
 export class CloseDateComponent implements OnInit {
+  dates = [
+    1,2,3,4,5
+  ]
 
   constructor() { }
 
