85b54979dcde1fdaf4e9fb828f92cdcd28dd4ab6
trang <trang@deha-soft.com>
2019-02-12 15:19:17 +0700

    update client detail

--- frontend/src/app/layout/client/detail/detail.component.ts
+++ frontend/src/app/layout/client/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+	clients = [
+		{
+			id: 123,
+			name: 'Giang Mai',
+			number: '123456789',
+			email: 'giang@mai.com',
+			gender: 'unknown'
+		},
+		{
+			id: 12375,
+			name: 'Giang Mai',
+			number: '123456789',
+			email: 'giang@mai.com',
+			gender: 'unknown'
+		},
+		{
+			id: 12345,
+			name: 'Giang Mai',
+			number: '123456789',
+			email: 'giang@mai.com',
+			gender: 'unknown'
+		},
+		{
+			id: 123234,
+			name: 'Giang Mai',
+			number: '123456789',
+			email: 'giang@mai.com',
+			gender: 'unknown'
+		},
+		{
+			id: 12354,
+			name: 'Giang Mai',
+			number: '123456789',
+			email: 'giang@mai.com',
+			gender: 'unknown'
+		},
+	];
 
-  ngOnInit() {
-  }
+	client_id: any;
+	client_info: any = {};
+
+  	constructor(private route: Router,
+  		private router: ActivatedRoute) { }
+
+	ngOnInit() {
+		this.router.params.subscribe(params => {this.client_id = params.id;});
+		this.loadDetails(this.client_id);
+	}
+
+	private loadDetails(id: any){
+		let info = this.clients.filter(s => s.id == id);
+		this.client_info = info[0];
+		console.log(this.client_info);
+	}
 
 }
