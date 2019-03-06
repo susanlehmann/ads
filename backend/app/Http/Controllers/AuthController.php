5d5e0365de7331ef8433faf87476e88ecda288e6
mai van giang <hoanggiang333889@gmail.com>
2019-03-06 18:11:50 +0700

    giang update

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'signup','verifyUserEmail','me']]);
+        $this->middleware('auth:api', ['except' => ['login', 'signup','verifyUserEmail','me','send_verifyEmail']]);
     }
 
     /**
